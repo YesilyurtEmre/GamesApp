14cf207361633278900988732e4d7ec69a24b8da
Emre Yesilyurt <yesilyurtemre@outlook.com.tr>
2025-03-20 15:12:23 +0300

    Initial folder structure and mock data integration

---
+++ GamesApp/App/TabBarController.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarController.swift
+//  GamesApp
+//
+//  Created by Emre Yeşilyurt on 19.03.2025.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        let gamesNav = createTab(
+            for: GamesVC(),
+            title: "Games",
+            imageName: "Games"
+        )
+        
+        let favouritesNav = createTab(
+            for: FavouritesVC(),
+            title: "Favourites",
+            imageName: "Favourite"
+        )
+        
+        viewControllers = [gamesNav, favouritesNav]
+    }
+    
+    private func createTab(for viewController: UIViewController,
+                           title: String,
+                           imageName: String) -> UINavigationController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem = UITabBarItem(
+            title: title,
+            image: UIImage(named: imageName),
+            selectedImage: UIImage(named: imageName)
+        )
+        return navController
+    }
+}
